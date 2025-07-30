32f88fdc75579d2cc6b07345d9a1224e00f176fe
FebryanHernanda <febryanhernanda@gmail.com>
2025-07-31 00:46:02 +0700

    refactor(organisms/newslatters): refactor component styling

--- src/components/organisms/newslatters/Newslatters.jsx
+++ src/components/organisms/newslatters/Newslatters.jsx
@@ -1,12 +1,12 @@
 const NewstLatters = () => {
   return (
     <section className="">
-      <div className="bg-blue-600 flex flex-col gap-5 justify-center rounded-2xl p-5 lg:p-10 relative ">
-        <h1 className="text-2xl text-center font-regular text-white">
+      <div className="bg-blue-600 flex flex-col gap-10 justify-center rounded-2xl px-5 py-20 lg:px-15 relative overflow-hidden ">
+        <h1 className="text-4xl text-center font-regular text-white">
           Subscribe to our newsletter
         </h1>
         <form className=" flex gap-5 flex-col lg:flex-row justify-center items-center">
-          <div className="flex gap-5 w-full lg:w-200 ">
+          <div className="flex gap-5 w-full lg:w-200">
             <input
               className="bg-white rounded-md p-2 w-full outline-none"
               type="text"
@@ -23,7 +23,7 @@ const NewstLatters = () => {
             />
           </div>
           <button
-            className="bg-white p-2 rounded-md text-blue-600 font-bold w-full md:w-50 "
+            className="bg-white p-2 rounded-md text-blue-600 font-bold w-full lg:w-50 "
             type="submit"
           >
             Subscribe
@@ -32,7 +32,7 @@ const NewstLatters = () => {
         <img
           src="/src/assets/icons/circle.svg"
           alt="background circle"
-          className="w-20 absolute right-0 bottom-0"
+          className="w-25 lg:w-40 absolute right-0 bottom-0"
         />
       </div>
     </section>
