1e8342298b85f603f3cf01e725c2aa497ea69e60
FebryanHernanda <febryanhernanda@gmail.com>
2025-07-30 22:11:34 +0700

    feat(organisms/newslatters): add newslatters component

---
+++ src/components/organisms/newslatters/Newslatters.jsx
@@ -0,0 +1,42 @@
+const NewstLatters = () => {
+  return (
+    <section className="">
+      <div className="bg-blue-600 flex flex-col gap-5 justify-center rounded-2xl p-5 lg:p-10 relative ">
+        <h1 className="text-2xl text-center font-regular text-white">
+          Subscribe to our newsletter
+        </h1>
+        <form className=" flex gap-5 flex-col lg:flex-row justify-center items-center">
+          <div className="flex gap-5 w-full lg:w-200 ">
+            <input
+              className="bg-white rounded-md p-2 w-full outline-none"
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Your name"
+            />
+            <input
+              className="bg-white rounded-md p-2 w-full outline-none "
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your email"
+            />
+          </div>
+          <button
+            className="bg-white p-2 rounded-md text-blue-600 font-bold w-full md:w-50 "
+            type="submit"
+          >
+            Subscribe
+          </button>
+        </form>
+        <img
+          src="/src/assets/icons/circle.svg"
+          alt="background circle"
+          className="w-20 absolute right-0 bottom-0"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default NewstLatters;
