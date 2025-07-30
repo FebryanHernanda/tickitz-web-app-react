de31dcf207ea9b0d784739d8866f9a58ec0d1e88
FebryanHernanda <febryanhernanda@gmail.com>
2025-07-31 00:45:27 +0700

    feat(organisms/navbar): add navigation and humberger menu

--- src/components/organisms/navbar/Navbar.jsx
+++ src/components/organisms/navbar/Navbar.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { MyButton } from "../../atoms";
+import { Menu } from "lucide-react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <header className="flex justify-between items-center p-5 shadow-md">
@@ -8,26 +17,32 @@ const Navbar = () => {
           src="/src/assets/icons/logo/tickitz-logo-blue.svg"
           alt="Tickitz Logo"
         />
-        <nav>
-          <ul className="flex gap-5">
-            <a href="#">
-              <li>Home</li>
-            </a>
-            <a href="#">
-              <li>Movie</li>
-            </a>
-            <a href="#">
-              <li>Buy Ticket</li>
-            </a>
+        <nav className={`lg:flex ${isMenuOpen ? "block" : "hidden"}`}>
+          <ul className="flex flex-row gap-5">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/movies">Movies</Link>
+            </li>
+            <li>
+              <Link to="#">Buy Tickets</Link>
+            </li>
           </ul>
         </nav>
 
-        <div className="flex gap-5">
-          <MyButton>Sign In</MyButton>
-          <MyButton>Sign Up</MyButton>
+        <div className="gap-5 hidden lg:flex">
+          <>
+            <MyButton>Sign In</MyButton>
+            <MyButton>Sign Up</MyButton>
+          </>
         </div>
+        <button className="flex lg:hidden" onClick={handleMenuToggle}>
+          <Menu />
+        </button>
       </header>
     </>
   );
 };
+
 export default Navbar;
