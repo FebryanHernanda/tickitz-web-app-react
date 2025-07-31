49bc1d4b065f9da8cdbb99932d9346c418389ac1
FebryanHernanda <febryanhernanda@gmail.com>
2025-07-31 09:44:49 +0700

    feat(routes): add paymentpages routes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LandingPages, MoviesPages } from "../components/pages";
+import { LandingPages, MoviesPages, PaymentPages } from "../components/pages";
 import { MainLayout } from "../layouts";
 
 function Router() {
@@ -9,6 +9,7 @@ function Router() {
         <Route element={<MainLayout />}>
           <Route path="/" element={<LandingPages />}></Route>
           <Route path="/movies" element={<MoviesPages />}></Route>
+          <Route path="/payment" element={<PaymentPages />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
