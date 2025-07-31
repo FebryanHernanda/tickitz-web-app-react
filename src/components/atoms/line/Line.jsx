9362850745ef47600b8f543af7bc3539eb5eeef3
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-01 00:05:37 +0700

    feat(atoms/line): add line progress component

---
+++ src/components/atoms/line/Line.jsx
@@ -0,0 +1,15 @@
+const Line = () => {
+  return (
+    <>
+      <div className="flex gap-1">
+        <div className="h-[1px] w-2 bg-gray-400"></div>
+        <div className="h-[1px] w-4 bg-gray-400"></div>
+        <div className="h-[1px] w-4 bg-gray-400"></div>
+        <div className="h-[1px] w-4 bg-gray-400"></div>
+        <div className="h-[1px] w-2 bg-gray-400"></div>
+      </div>
+    </>
+  );
+};
+
+export default Line;
