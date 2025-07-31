ea52494dbef168147e4e1e627a9e2f7091b61ee5
FebryanHernanda <febryanhernanda@gmail.com>
2025-07-31 13:25:04 +0700

    feat(layouts/authlayouts): add outlet router

--- src/layouts/AuthLayout/AuthLayout.jsx
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -1,19 +1,23 @@
-const AuthLayout = (props) => {
+import { Outlet } from "react-router-dom";
+
+const AuthLayout = () => {
   return (
-    <div
-      className=" w-full min-h-screen bg-cover bg-center bg-no-repeat flex flex-col justify-center items-center gap-10 p-5 lg:p-10"
-      style={{
-        backgroundImage:
-          "linear-gradient(rgba(0, 0, 0, 0.3)), url(src/assets/background/background.png)",
-      }}
-    >
-      <img
-        src="/src/assets/icons/logo/tickitz-logo-white.svg"
-        alt="Tickitz Logo"
-        className="w-100"
-      />
-      <div className="">{props.children}</div>;
-    </div>
+    <>
+      <div
+        className=" w-full min-h-screen bg-cover bg-center bg-no-repeat flex flex-col justify-center items-center gap-10 p-5 lg:p-10"
+        style={{
+          backgroundImage:
+            "linear-gradient(rgba(0, 0, 0, 0.3)), url(../src/assets/background/background.png)",
+        }}
+      >
+        <img
+          src="/src/assets/icons/logo/tickitz-logo-white.svg"
+          alt="Tickitz Logo"
+          className="w-100"
+        />
+        <Outlet />
+      </div>
+    </>
   );
 };
 
