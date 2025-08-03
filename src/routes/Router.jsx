041f01ab21fd9b4ae8634535fec9ab89125a1e03
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-04 00:11:18 +0700

    feat(routes): add private routes, define the component routes and  connect all routes pages

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
+  AdminDashboard,
+  AdminData,
+  AdminMovieForm,
   LandingPages,
   LoginPages,
   MoviesPages,
+  OrderPages,
   PaymentPages,
+  ProfilePages,
   RegisterPages,
+  ResultsPages,
 } from "../components/pages";
 import { AuthLayout, MainLayout } from "../layouts";
+import MoviesDetailsPages from "../components/pages/movies/MoviesDetailsPages";
+import PrivateRoutes from "./PrivateRoutes";
 
 function Router() {
   return (
@@ -14,8 +22,39 @@ function Router() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<LandingPages />}></Route>
-          <Route path="movies" element={<MoviesPages />}></Route>
-          <Route path="payment" element={<PaymentPages />}></Route>
+
+          {/* Movies */}
+          <Route path="movies">
+            <Route index element={<MoviesPages />}></Route>
+            <Route
+              path="details/:movieId"
+              element={<MoviesDetailsPages />}
+            ></Route>
+          </Route>
+          {/* Movies */}
+
+          {/* Admin */}
+          <Route path="admin">
+            <Route index element={<AdminDashboard />}></Route>
+            <Route path="data" element={<AdminData />}></Route>
+            <Route path="data/add-movie" element={<AdminMovieForm />}></Route>
+          </Route>
+          {/* Admin */}
+
+          {/* Private Routes */}
+          <Route element={<PrivateRoutes />}>
+            <Route path="payment">
+              <Route index element={<PaymentPages />}></Route>
+              <Route path="results" element={<ResultsPages />}></Route>
+            </Route>
+
+            <Route path="order" element={<OrderPages />}></Route>
+
+            <Route path="profile">
+              <Route index element={<ProfilePages />}></Route>
+            </Route>
+          </Route>
+          {/* Private Routes */}
         </Route>
 
         <Route path="auth" element={<AuthLayout />}>
