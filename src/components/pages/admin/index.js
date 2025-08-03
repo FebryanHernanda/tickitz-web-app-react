9b5a71e20e25317a1600f7f37be51801fecacb09
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-03 23:39:56 +0700

    feat(pages/admin): add export file component from admin pages

---
+++ src/components/pages/admin/index.js
@@ -0,0 +1,3 @@
+export { default as AdminDashboard } from "./AdminDashboard.jsx";
+export { default as AdminData } from "./AdminData.jsx";
+export { default as AdminMovieForm } from "./AdminMovieForm.jsx";
