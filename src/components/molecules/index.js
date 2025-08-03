c3150435ad7826e8afa076e5da86ec5a0b99c9d2
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-03 23:24:05 +0700

    feat(molecules/modal): add modal payment confirmations

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,3 +2,4 @@ export * from "./formField";
 export * from "./buttonSocialMedia";
 export * from "./card";
 export * from "./modal";
+export * from "./tickethistory";
--- src/components/molecules/modal/ModalPayment.jsx
+++ src/components/molecules/modal/ModalPayment.jsx
@@ -1,13 +1,27 @@
 // components/molecules/PaymentModal.jsx
+import { X } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 
 const ModalPayment = (props) => {
-  const { isOpen, onClose } = props;
   const navigate = useNavigate();
+
+  /* props data */
+  const { isOpen, prices, data, onClose } = props;
+
   if (!isOpen) return null;
+
+  /* Handle Payment */
+  const handlePayment = () => {
+    navigate("results", {
+      state: {
+        data,
+      },
+    });
+  };
 
   return (
     <div className="absolute inset-0 flex items-center justify-center bg-black/50">
+      {/* Container */}
       <div className="relative max-w-md rounded-xl bg-white p-6 shadow-lg">
         <div className="mb-4">
           <h1 className="mb-2 text-xl font-semibold">Payment Info</h1>
@@ -24,26 +38,24 @@ const ModalPayment = (props) => {
 
           <div className="mb-4">
             <h4 className="text-sm text-gray-500">Total Payment</h4>
-            <h3 className="text-lg text-blue-600">$30</h3>
+            <h3 className="text-lg text-blue-600">{prices}</h3>
           </div>
 
           <p className="text-sm leading-relaxed text-gray-500">
             Pay this payment bill before it is due,
-            <span className="font-medium text-red-500"> on June 23, 2023</span>.
-            If the bill has not been paid by the specified time, it will be
+            <span className="font-medium text-red-500">{` on ${data.dateShow}`}</span>
+            . If the bill has not been paid by the specified time, it will be
             forfeited.
           </p>
         </div>
 
         <div className="flex justify-end gap-3">
-          {/* <Link to="/pages/payment/ticket-results.html"> */}
           <button
-            onClick={() => navigate("results")}
+            onClick={handlePayment}
             className="rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700"
           >
             Check Payment
           </button>
-          {/* </Link> */}
           <button
             onClick={onClose}
             className="rounded-md border border-gray-300 px-4 py-2 transition hover:bg-gray-100"
@@ -56,7 +68,7 @@ const ModalPayment = (props) => {
           onClick={onClose}
           className="absolute top-2 right-3 text-lg text-gray-400 hover:text-black"
         >
-          &times;
+          <X />
         </button>
       </div>
     </div>
