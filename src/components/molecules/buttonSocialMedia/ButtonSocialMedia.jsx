585d08ef28cbc9d791b910875b1e72c82d2434ac
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-04 10:55:43 +0700

    refactor(molecules/buttonSocialMedia): change source  media from src/assets using import system

--- src/components/molecules/buttonSocialMedia/ButtonSocialMedia.jsx
+++ src/components/molecules/buttonSocialMedia/ButtonSocialMedia.jsx
@@ -1,21 +1,16 @@
+import facebookLogo from "/src/assets/icons/socialmedia/facebook2-icon.svg";
+import googleLogo from "/src/assets/icons/socialmedia/google-icon.svg";
+
 const ButtonSocialMedia = () => {
   return (
     <>
       <div className="flex justify-between">
         <button className="flex items-center justify-center gap-5 rounded-xl border-1 border-gray-300 p-3 shadow-md hover:bg-gray-100 lg:w-50">
-          <img
-            src="/src/assets/icons/socialmedia/google-icon.svg"
-            alt="Google Icon"
-            className="w-10"
-          />
+          <img src={googleLogo} alt="Google Icon" className="w-10" />
           Google
         </button>
         <button className="flex items-center justify-center gap-5 rounded-xl border-1 border-gray-300 p-3 shadow-md hover:bg-gray-100 lg:w-50">
-          <img
-            src="/src/assets/icons/socialmedia/facebook2-icon.svg"
-            alt="Facebook Icon"
-            className="w-10"
-          />
+          <img src={facebookLogo} alt="Facebook Icon" className="w-10" />
           Facebook
         </button>
       </div>
