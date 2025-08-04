0766dc6daba52391bab8e8ad74d4076548d33f00
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-04 11:02:59 +0700

    refactor(layouts/authlayout): change source media from src/assets using import system

--- src/layouts/AuthLayout/AuthLayout.jsx
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -1,20 +1,18 @@
 import { Outlet } from "react-router-dom";
 
+import heroBg from "/src/assets/background/background.png";
+import ticketLogoWhite from "/src/assets/icons/logo/tickitz-logo-white.svg";
+
 const AuthLayout = () => {
   return (
     <>
       <div
         className="flex min-h-screen w-full flex-col items-center justify-center gap-10 bg-cover bg-center bg-no-repeat p-5 lg:p-10"
         style={{
-          backgroundImage:
-            "linear-gradient(rgba(0, 0, 0, 0.3)), url(../src/assets/background/background.png)",
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3)), url(${heroBg})`,
         }}
       >
-        <img
-          src="/src/assets/icons/logo/tickitz-logo-white.svg"
-          alt="Tickitz Logo"
-          className="w-100"
-        />
+        <img src={ticketLogoWhite} alt="Tickitz Logo" className="w-100" />
         <Outlet />
       </div>
     </>
