f66e1337746fc227f954722a6c11c04c01510b44
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-04 10:56:30 +0700

    refactor(organisms/footer): change source  media from src/assets using import system

--- src/components/organisms/footer/Footer.jsx
+++ src/components/organisms/footer/Footer.jsx
@@ -1,14 +1,21 @@
+import logoBlue from "/src/assets/icons/logo/tickitz-logo-blue.svg";
+
+import ebvLogo from "/src/assets/icons/sponsor/ebv-logo.svg";
+import cineOneLogo from "/src/assets/icons/sponsor/CineOne-logo.svg";
+import hiflixLogo from "/src/assets/icons/sponsor/hiflix-logo.svg";
+
+import facebookIcon from "/src/assets/icons/socialmedia/facebook-icon.svg";
+import instagramIcon from "/src/assets/icons/socialmedia/instagram-icon.svg";
+import twitterIcon from "/src/assets/icons/socialmedia/twitter-icon.svg";
+import youtubeIcon from "/src/assets/icons/socialmedia/youtube-icon.svg";
+
 const Footer = () => {
   return (
     <>
       <footer className="p-5 lg:p-10">
         <div className="flex flex-col gap-10 lg:flex-row lg:justify-between">
           <div className="flex flex-col gap-5">
-            <img
-              src="/src/assets/icons/logo/tickitz-logo-blue.svg"
-              alt="Tickitz Logo"
-              className="w-50"
-            />
+            <img src={logoBlue} alt="Tickitz Logo" className="w-50" />
             <p className="text-small font-light">
               Stop waiting in line. Buy tickets conveniently, watch movies
               quietly.
@@ -37,22 +44,13 @@ const Footer = () => {
             <h3 className="font-bold">Our Sponsor</h3>
             <ul className="flex flex-wrap items-end justify-between gap-10 md:justify-normal lg:flex-col lg:flex-nowrap lg:items-start">
               <li>
-                <img
-                  src="/src/assets/icons/sponsor/ebv-logo.svg"
-                  alt="EBV Sponsor Logo"
-                />
+                <img src={ebvLogo} alt="EBV Sponsor Logo" />
               </li>
               <li>
-                <img
-                  src="/src/assets/icons/sponsor/CineOne-logo.svg"
-                  alt="CineOne Sponsor Logo"
-                />
+                <img src={cineOneLogo} alt="CineOne Sponsor Logo" />
               </li>
               <li>
-                <img
-                  src="/src/assets/icons/sponsor/hiflix-logo.svg"
-                  alt="hiflix Sponsor Logo"
-                />
+                <img src={hiflixLogo} alt="hiflix Sponsor Logo" />
               </li>
             </ul>
           </div>
@@ -61,31 +59,19 @@ const Footer = () => {
             <h3 className="font-bold">Follow Us</h3>
             <ul className="flex flex-wrap gap-5 lg:flex-col">
               <li className="flex items-center gap-2">
-                <img
-                  src="/src/assets/icons/socialmedia/facebook-icon.svg"
-                  alt="Facebook Logo"
-                />
+                <img src={facebookIcon} alt="Facebook Logo" />
                 <a href="#">Tickitz Cinema id</a>
               </li>
               <li className="flex items-center gap-2">
-                <img
-                  src="/src/assets/icons/socialmedia/instagram-icon.svg"
-                  alt="Instagram Logo"
-                />
+                <img src={instagramIcon} alt="Instagram Logo" />
                 <a href="#">tickitz.id</a>
               </li>
               <li className="flex items-center gap-2">
-                <img
-                  src="/src/assets/icons/socialmedia/twitter-icon.svg"
-                  alt="twitter Logo"
-                />
+                <img src={twitterIcon} alt="twitter Logo" />
                 <a href="#">tickitz.id</a>
               </li>
               <li className="flex items-center gap-2">
-                <img
-                  src="/src/assets/icons/socialmedia/youtube-icon.svg"
-                  alt="Youtube Logo"
-                />
+                <img src={youtubeIcon} alt="Youtube Logo" />
                 <a href="#">Tickitz Cinema id</a>
               </li>
             </ul>
