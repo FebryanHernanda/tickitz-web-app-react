a360e9974e4e6df2db3e20d73850f75b4216116f
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-08 23:22:25 +0700

    feat(routes): add toascontainer global component

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -16,56 +16,60 @@ import {
 import { AuthLayout, MainLayout } from "../layouts";
 
 import PrivateRoutes from "./PrivateRoutes";
+import { ToastContainer } from "react-toastify";
 // query params
 // paginations
 
 function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<LandingPages />}></Route>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<LandingPages />}></Route>
 
-          {/* Movies */}
-          <Route path="movies">
-            <Route index element={<MoviesPages />}></Route>
-            <Route
-              path="details/:movieId"
-              element={<MoviesDetailsPages />}
-            ></Route>
-          </Route>
-          {/* Movies */}
-
-          {/* Admin */}
-          <Route path="admin">
-            <Route index element={<AdminDashboard />}></Route>
-            <Route path="data" element={<AdminData />}></Route>
-            <Route path="data/add-movie" element={<AdminMovieForm />}></Route>
-          </Route>
-          {/* Admin */}
-
-          {/* Private Routes */}
-          <Route element={<PrivateRoutes />}>
-            <Route path="payment">
-              <Route index element={<PaymentPages />}></Route>
-              <Route path="results" element={<ResultsPages />}></Route>
+            {/* Movies */}
+            <Route path="movies">
+              <Route index element={<MoviesPages />}></Route>
+              <Route
+                path="details/:movieId"
+                element={<MoviesDetailsPages />}
+              ></Route>
             </Route>
+            {/* Movies */}
 
-            <Route path="order" element={<OrderPages />}></Route>
-
-            <Route path="profile">
-              <Route index element={<ProfilePages />}></Route>
+            {/* Admin */}
+            <Route path="admin">
+              <Route index element={<AdminDashboard />}></Route>
+              <Route path="data" element={<AdminData />}></Route>
+              <Route path="data/add-movie" element={<AdminMovieForm />}></Route>
             </Route>
-          </Route>
-          {/* Private Routes */}
-        </Route>
+            {/* Admin */}
 
-        <Route path="auth" element={<AuthLayout />}>
-          <Route path="login" element={<LoginPages />}></Route>
-          <Route path="register" element={<RegisterPages />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+            {/* Private Routes */}
+            <Route element={<PrivateRoutes />}>
+              <Route path="payment">
+                <Route index element={<PaymentPages />}></Route>
+                <Route path="results" element={<ResultsPages />}></Route>
+              </Route>
+
+              <Route path="order" element={<OrderPages />}></Route>
+
+              <Route path="profile">
+                <Route index element={<ProfilePages />}></Route>
+              </Route>
+            </Route>
+            {/* Private Routes */}
+          </Route>
+
+          <Route path="auth" element={<AuthLayout />}>
+            <Route path="login" element={<LoginPages />}></Route>
+            <Route path="register" element={<RegisterPages />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
   );
 }
 
