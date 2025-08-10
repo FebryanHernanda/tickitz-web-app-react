54606d4f9f674c34cfe80889cdc0b68947f5e3b6
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-11 01:16:50 +0700

    feat(molecules/timepicker): add handle submit and props for get time data

--- src/components/molecules/timePicker/TimePicker.jsx
+++ src/components/molecules/timePicker/TimePicker.jsx
@@ -1,12 +1,27 @@
-const TimePicker = ({ isOpen }) => {
+const TimePicker = ({ isOpen, setMovieForm }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const startTime = e.target.startTime.value;
+    const times = e.target.times.value;
+    const fulltime = `${startTime} ${times}`;
+
+    setMovieForm((prevData) => ({
+      ...prevData,
+      showTime: [...prevData.showTime, fulltime],
+    }));
+
+    isOpen(false);
+  };
+
   return (
-    <div
+    <form
       id="dropdownTimepicker"
       className="flex w-auto flex-col items-center gap-5 rounded-lg border-1 border-gray-400 bg-white p-3 shadow-2xl"
+      onSubmit={handleSubmit}
     >
       <div className="flex flex-col gap-5 text-center">
         <label
-          htmlFor="start-time"
+          htmlFor="startTime"
           className="text-md w-50 text-lg font-medium text-blue-900"
         >
           Movies Time:
@@ -14,10 +29,9 @@ const TimePicker = ({ isOpen }) => {
         <div className="flex justify-between gap-2.5">
           <input
             type="time"
-            id="start-time"
+            id="startTime"
+            name="startTime"
             className="w-full rounded-lg border border-gray-400 p-2.5 text-sm leading-none text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-            min="09:00"
-            max="18:00"
             defaultValue="00:00"
             required
           />
@@ -29,13 +43,12 @@ const TimePicker = ({ isOpen }) => {
       </div>
       <button
         id="saveTimeButton"
-        type="button"
+        type="submit"
         className="flex items-center rounded-lg bg-blue-700 p-1 px-5 text-white"
-        onClick={isOpen}
       >
         Save time
       </button>
-    </div>
+    </form>
   );
 };
 
