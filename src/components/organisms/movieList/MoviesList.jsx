4ab092b259954b87ddd8ac39c2a3e49eb8d7bcd2
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-11 01:19:59 +0700

    feat(organisms/movieslist): migrate fetching data to redux

--- src/components/organisms/movieList/MoviesList.jsx
+++ src/components/organisms/movieList/MoviesList.jsx
@@ -1,33 +1,6 @@
-import { useEffect, useState } from "react";
 import { MovieCard } from "../../molecules";
-import { getGenreMovies, getPopularMovies } from "../../../data";
 
-const MovieList = ({ limits }) => {
-  const apiKey = import.meta.env.VITE_META_API_KEY;
-  const [movies, setMovies] = useState([]);
-  const [genres, setGenres] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchAll = async () => {
-      try {
-        const [moviesData, genresData] = await Promise.all([
-          getPopularMovies(apiKey),
-          getGenreMovies(apiKey),
-        ]);
-
-        setMovies(moviesData);
-        setGenres(genresData);
-      } catch (error) {
-        console.error("Fetch error:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchAll();
-  }, [apiKey]);
-
+const MovieList = ({ limits, movies, genres, loading }) => {
   const displayedMovies = limits ? movies.slice(0, limits) : movies;
 
   return (
