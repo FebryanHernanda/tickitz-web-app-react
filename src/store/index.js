0ec1c6b5a87f155fc209798209536350e4211313
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-11 01:29:46 +0700

    feat(store): integrate redux persist and config the store

---
+++ src/store/index.js
@@ -0,0 +1,45 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  PERSIST,
+  REHYDRATE,
+  REGISTER,
+  FLUSH,
+  PAUSE,
+  PURGE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import moviesReducers from "./slices/moviesSlice";
+import adminReducers from "./slices/adminSlice";
+
+// combine all reducers
+const rootReducer = combineReducers({
+  admin: adminReducers,
+  movies: moviesReducers,
+});
+
+// persist configuration
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// warp rootreducer with persist
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// create store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [PERSIST, REHYDRATE, REGISTER, FLUSH, PAUSE, PURGE],
+      },
+    });
+  },
+});
+
+// create persistor
+export const persistor = persistStore(store);
