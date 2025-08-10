084d3eaf1faca3fe895346c9883376d63c7541cf
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-11 01:18:06 +0700

    feat(organisms/chart): add salesChart and ticketSalesChart component

--- src/components/organisms/chart/SalesChart.jsx
+++ src/components/organisms/chart/SalesChart.jsx
@@ -1,30 +1,116 @@
+import { Line } from "react-chartjs-2";
+import { getDataSalesByCategory } from "../../../data/chart/salesData";
+import { useEffect, useState } from "react";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from "chart.js";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovies } from "../../../store/slices/moviesSlice";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+);
+
 const SalesChart = () => {
+  const dispatch = useDispatch();
+  const { movies } = useSelector((state) => state.movies);
+
+  const [moviesName, setMoviesName] = useState("All Movies");
+  const [timeCategory, setTimeCategory] = useState("monthly");
+  const [chartData, setChartData] = useState(getDataSalesByCategory("monthly"));
+
+  useEffect(() => {
+    if (!movies.length) dispatch(fetchMovies({ page: 1 }));
+  }, [dispatch, movies]);
+
+  const handleTimeChange = (e) => setTimeCategory(e.target.value);
+  const handleMoviesName = (e) => setMoviesName(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setChartData(getDataSalesByCategory(timeCategory));
+  };
+
+  const optionsSales = {
+    responsive: true,
+    plugins: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: moviesName,
+        align: "start",
+        marginBottom: 20,
+        font: {
+          size: 18,
+          weight: "bold",
+        },
+        padding: {
+          top: 10,
+          bottom: 30,
+          left: 0,
+          right: 0,
+        },
+      },
+    },
+  };
+
   return (
-    <section className="sales-chart flex flex-col gap-5 rounded-[20px] bg-white !p-5">
+    <section
+      className="flex h-full w-full flex-col gap-5 rounded-[20px] bg-white !p-5 xl:w-1/2"
+      onSubmit={handleSubmit}
+    >
       <h1 className="text-xl">Sales Chart</h1>
 
       <form className="flex gap-5 !pt-2" id="salesChartForm">
         {/* Movies Name */}
-        <label
-          htmlFor="movies-name"
-          className="rounded-[8px] bg-[#EFF0F6] !p-1"
-        >
+        <label htmlFor="moviesname" className="rounded-[8px] bg-[#EFF0F6] !p-1">
           <select
-            name="movies-name"
-            id="movies-name"
+            name="moviesName"
+            id="moviesName"
             className="w-[100px] md:w-[220px]"
+            value={moviesName}
+            onChange={handleMoviesName}
           >
-            <option value="">Movies Name</option>
-            <option value="How to Train Your Dragon">
-              How to Train Your Dragon
+            <option value="" disabled>
+              Movies Name
             </option>
-            <option value="Man with No Past">Man with No Past</option>
+            {movies.map((item) => {
+              return (
+                <option key={item.id} value={item.title}>
+                  {item.title}
+                </option>
+              );
+            })}
+
+            {/* <option value="Man with No Past">Man with No Past</option> */}
           </select>
         </label>
 
         {/* Time */}
         <label htmlFor="time" className="rounded-[8px] bg-[#EFF0F6] !p-1">
-          <select name="time" id="time" className="w-[100px]">
+          <select
+            name="time"
+            id="time"
+            className="w-[100px]"
+            value={timeCategory}
+            onChange={handleTimeChange}
+          >
             <option value="yearsly">Yearsly</option>
             <option value="monthly">Monthly</option>
             <option value="weekly">Weekly</option>
@@ -39,7 +125,9 @@ const SalesChart = () => {
         </button>
       </form>
 
-      <div className="w-full"></div>
+      <div className="w-full">
+        <Line options={optionsSales} data={chartData} />
+      </div>
     </section>
   );
 };
--- src/components/organisms/chart/TicketSalesChart.jsx
+++ src/components/organisms/chart/TicketSalesChart.jsx
@@ -1,9 +1,78 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from "chart.js";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchMovies } from "../../../store/slices/moviesSlice";
+import { Line } from "react-chartjs-2";
+import { useState } from "react";
+import { getDataTicketSalesByCategory } from "../../../data/chart/ticketSalesData";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+);
+
 const TicketSalesChart = () => {
+  const [categoryName, setCategoryName] = useState("Horror");
+  const [locationName, setLocationName] = useState("Jakarta");
+  const [chartData, setChartData] = useState(
+    getDataTicketSalesByCategory("Jakarta"),
+  );
+
+  const handleLocationName = (e) => setLocationName(e.target.value);
+  const handleCategoryName = (e) => setCategoryName(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setChartData(getDataTicketSalesByCategory(locationName));
+  };
+
+  const optionsSales = {
+    responsive: true,
+    plugins: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: `${categoryName}, ${locationName}`,
+        align: "start",
+        marginBottom: 20,
+        font: {
+          size: 18,
+          weight: "bold",
+        },
+        padding: {
+          top: 10,
+          bottom: 30,
+          left: 0,
+          right: 0,
+        },
+      },
+    },
+  };
+
   return (
     <>
-      <section className="ticket-sales flex flex-col gap-5 rounded-[20px] bg-white !p-5">
+      <section className="flex h-full w-full flex-col gap-5 rounded-[20px] bg-white !p-5 xl:w-1/2">
         <h1 className="text-xl">Ticket Sales</h1>
-        <form className="flex gap-5 pt-2" id="ticketSalesForm">
+        <form
+          className="flex gap-5 pt-2"
+          id="ticketSalesForm"
+          onSubmit={handleSubmit}
+        >
           {/* <!-- category Name --> */}
           <label
             htmlFor="category-name"
@@ -13,17 +82,32 @@ const TicketSalesChart = () => {
               name="category-name"
               id="category-name"
               className="w-[100px] md:w-[220px]"
+              value={categoryName}
+              onChange={handleCategoryName}
             >
-              <option value="">Category</option>
+              <option value="" disabled>
+                Category
+              </option>
               <option value="Action">Action</option>
               <option value="Adventure">Adventure</option>
+              <option value="Horror">Horror</option>
+              <option value="Romance">Romance</option>
+              <option value="Sci-Fi">Sci-Fi</option>
             </select>
           </label>
 
           {/* <!-- times --> */}
           <label htmlFor="location" className="rounded-[8px] bg-[#EFF0F6] !p-1">
-            <select name="location" id="location" className="w-[100px]">
-              <option value="">Location</option>
+            <select
+              name="location"
+              id="location"
+              className="w-[100px]"
+              value={locationName}
+              onChange={handleLocationName}
+            >
+              <option value="" disabled>
+                Location
+              </option>
               <option value="Jakarta">Jakarta</option>
               <option value="Bandung">Bandung</option>
               <option value="Bogor">Bogor</option>
@@ -34,7 +118,9 @@ const TicketSalesChart = () => {
           </button>
         </form>
 
-        <div className="w-full"></div>
+        <div className="w-full">
+          <Line options={optionsSales} data={chartData} />
+        </div>
       </section>
     </>
   );
