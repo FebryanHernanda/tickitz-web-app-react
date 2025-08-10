88da821e63be146b4bc9d8c533101f89b3241886
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-11 01:24:49 +0700

    fix(organisms/newslatter): change to semantic html

--- src/components/pages/admin/AdminDashboard.jsx
+++ src/components/pages/admin/AdminDashboard.jsx
@@ -2,12 +2,12 @@ import { SalesChart, TicketSalesChart } from "../../organisms";
 
 const AdminDashboard = () => {
   return (
-    <div className="bg-gray-200">
-      <div className="mx-auto flex max-w-screen-2xl flex-col gap-10 p-5 lg:p-10">
+    <section className="bg-gray-200">
+      <div className="mx-auto flex min-h-screen max-w-screen-2xl flex-col items-center gap-10 p-5 lg:p-10 xl:flex-row">
         <SalesChart />
         <TicketSalesChart />
       </div>
-    </div>
+    </section>
   );
 };
 
