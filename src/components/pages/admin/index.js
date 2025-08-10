15972c14df828d804c5215dfdd7a8c6f4723d042
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-11 01:26:27 +0700

    feat(pages/admin): add adminEditForm component path

--- src/components/pages/admin/index.js
+++ src/components/pages/admin/index.js
@@ -1,3 +1,4 @@
 export { default as AdminDashboard } from "./AdminDashboard.jsx";
 export { default as AdminData } from "./AdminData.jsx";
 export { default as AdminMovieForm } from "./AdminMovieForm.jsx";
+export { default as AdminEditForm } from "./AdminEditForm.jsx";
