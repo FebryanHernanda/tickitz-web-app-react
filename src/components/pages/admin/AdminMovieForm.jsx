ea68734d0d75a022dd0e2fd67cee7a24942703c6
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-11 01:26:13 +0700

    feat(pages/admin): add adminMovieForm component

--- src/components/pages/admin/AdminMovieForm.jsx
+++ src/components/pages/admin/AdminMovieForm.jsx
@@ -1,12 +1,103 @@
-import { UploadCloud } from "lucide-react";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { X } from "lucide-react";
+import { toast } from "react-toastify";
+
 import { TimePicker } from "../../molecules";
+import { addMoviesData } from "../../../store/slices/adminSlice";
 
 const AdminMovieForm = () => {
+  const dispatch = useDispatch();
   const [inputTime, setInputTime] = useState(false);
+  const { dataMovies } = useSelector((state) => state.admin);
+
+  const [movieForm, setMovieForm] = useState({
+    thumbnail:
+      "https://lh6.googleusercontent.com/proxy/SbyqrAadZJMr0OnkeMsUBHNdWB4qiB0i4MZKRvOjyvBs6Ul-GS0U3uuQV9H787LBxoshDNR1uO1rtk5xToQ9CcU1QySX5YTIQ-vLCG5ZRe6AvT4cPv5ITUZKB70IdAM",
+    name: "",
+    category: "",
+    releaseDate: "",
+    durationHours: "",
+    durationMinutes: "",
+    directorName: "",
+    cast: "",
+    synopsis: "",
+    location: "",
+    movieDate: "",
+    showTime: [],
+  });
+  const [errorMsg, setErrorMsg] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  /* Handle onChange */
+  const handleDataChange = (e) => {
+    const { name, value } = e.target;
+
+    setIsValid(value.length > 0);
+
+    setMovieForm((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  /* Handle Delete Time */
+  const handleDeleteTime = (id) => {
+    console.log("masuk");
+    const showTimeUpdate = movieForm.showTime.filter((_, idx) => idx !== id);
+
+    setMovieForm((prevData) => ({
+      ...prevData,
+      showTime: showTimeUpdate,
+    }));
+  };
+
+  /* Handle Submit */
+  const handleSubmit = () => {
+    const newId = dataMovies.length + 1;
+    if (movieForm.showTime.length === 0) {
+      setErrorMsg("Mohon mengisikan waktu penayangan !");
+      return;
+    }
+
+    if (!isValid) {
+      setErrorMsg("Mohon isi seluruh kolom field !");
+      return;
+    }
+
+    const movieData = {
+      ...movieForm,
+      id: newId,
+    };
+
+    dispatch(addMoviesData(movieData));
+
+    /* Reset Form */
+    setMovieForm({
+      thumbnail:
+        "https://lh6.googleusercontent.com/proxy/SbyqrAadZJMr0OnkeMsUBHNdWB4qiB0i4MZKRvOjyvBs6Ul-GS0U3uuQV9H787LBxoshDNR1uO1rtk5xToQ9CcU1QySX5YTIQ-vLCG5ZRe6AvT4cPv5ITUZKB70IdAM",
+      name: "",
+      category: "",
+      releaseDate: "",
+      durationHours: "",
+      durationMinutes: "",
+      directorName: "",
+      cast: "",
+      synopsis: "",
+      location: "",
+      movieDate: "",
+      showTime: [],
+    });
+
+    toast.success("Data Berhasil ditambahkan!", {
+      position: "top-center",
+      autoClose: 2000,
+    });
+  };
 
   const handleInputTime = (e) => {
     e.stopPropagation();
+    setErrorMsg("");
     setInputTime((prev) => !prev);
   };
 
@@ -32,6 +123,10 @@ const AdminMovieForm = () => {
             <input
               type="text"
               className="w-full rounded-md border border-gray-400 px-3 py-2 text-sm"
+              name="name"
+              value={movieForm.name}
+              onChange={handleDataChange}
+              required
             />
           </div>
           {/* Movie Name */}
@@ -42,6 +137,10 @@ const AdminMovieForm = () => {
             <input
               type="text"
               className="w-full rounded-md border border-gray-400 px-3 py-2 text-sm"
+              name="category"
+              value={movieForm.category}
+              onChange={handleDataChange}
+              required
             />
           </div>
           {/* Category */}
@@ -53,6 +152,10 @@ const AdminMovieForm = () => {
               <input
                 type="date"
                 className="w-full rounded-md border border-gray-400 px-3 py-2 text-sm"
+                name="releaseDate"
+                value={movieForm.releaseDate}
+                onChange={handleDataChange}
+                required
               />
             </div>
             {/* Release Date */}
@@ -67,11 +170,21 @@ const AdminMovieForm = () => {
                   type="number"
                   className="w-1/2 rounded-md border border-gray-400 px-3 py-2 text-sm"
                   placeholder="Hours"
+                  name="durationHours"
+                  value={movieForm.durationHours}
+                  onChange={handleDataChange}
+                  max={10}
+                  required
                 />
                 <input
                   type="number"
                   className="w-1/2 rounded-md border border-gray-400 px-3 py-2 text-sm"
                   placeholder="Minutes"
+                  name="durationMinutes"
+                  value={movieForm.durationMinutes}
+                  onChange={handleDataChange}
+                  max={60}
+                  required
                 />
               </div>
             </div>
@@ -84,6 +197,10 @@ const AdminMovieForm = () => {
             <input
               type="text"
               className="w-full rounded-md border border-gray-400 px-3 py-2 text-sm"
+              name="directorName"
+              value={movieForm.directorName}
+              onChange={handleDataChange}
+              required
             />
           </div>
           {/* Director Name */}
@@ -94,6 +211,10 @@ const AdminMovieForm = () => {
             <input
               type="text"
               className="w-full rounded-md border border-gray-400 px-3 py-2 text-sm"
+              name="cast"
+              value={movieForm.cast}
+              onChange={handleDataChange}
+              required
             />
           </div>
           {/* Cast */}
@@ -104,6 +225,10 @@ const AdminMovieForm = () => {
             <textarea
               rows="3"
               className="w-full rounded-md border border-gray-400 px-3 py-2 text-sm"
+              name="synopsis"
+              value={movieForm.synopsis}
+              onChange={handleDataChange}
+              required
             ></textarea>
           </div>
           {/* Synopsis */}
@@ -114,6 +239,10 @@ const AdminMovieForm = () => {
             <input
               type="text"
               className="w-full rounded-md border border-gray-400 px-3 py-2 text-sm"
+              name="location"
+              value={movieForm.location}
+              onChange={handleDataChange}
+              required
             />
           </div>
           {/* Add Location */}
@@ -125,6 +254,10 @@ const AdminMovieForm = () => {
               <input
                 type="date"
                 className="w-1/3 rounded-md border border-gray-400 px-3 py-2 text-sm"
+                name="movieDate"
+                value={movieForm.movieDate}
+                onChange={handleDataChange}
+                required
               />
               <div className="flex items-center gap-10">
                 <button
@@ -138,28 +271,40 @@ const AdminMovieForm = () => {
                   className="absolute top-35"
                   onClick={(e) => e.stopPropagation()}
                 >
-                  {inputTime && <TimePicker isOpen={handleInputTime} />}
+                  {inputTime && (
+                    <TimePicker
+                      isOpen={setInputTime}
+                      setMovieForm={setMovieForm}
+                    />
+                  )}
                 </div>
-
                 <div className="flex gap-10">
-                  <h3>08:30 AM</h3>
-                  <h3>13:30 PM</h3>
-                  <h3>20:30 PM</h3>
+                  {movieForm.showTime.length > 0 &&
+                    movieForm.showTime.map((data, id) => {
+                      return (
+                        <span key={id} className="flex items-center gap-5">
+                          <h3>{data}</h3>
+                          <X size={20} onClick={() => handleDeleteTime(id)} />
+                        </span>
+                      );
+                    })}
                 </div>
               </div>
             </div>
           </div>
           {/* Start Date & Time */}
-
           {/* Submit Button */}
           <button
-            type="button"
+            type="submit"
             className="w-full rounded-md bg-blue-600 py-2 text-sm text-white hover:bg-blue-700"
+            onClick={handleSubmit}
           >
             Save Movie
           </button>
-
           {/* Submit Button */}
+          {!isValid && (
+            <span className="text-center text-red-500">{errorMsg}</span>
+          )}
         </div>
       </div>
     </section>
