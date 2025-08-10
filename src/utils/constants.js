aad72e3ce3555383970aed0df37c7448db08f70e
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-11 01:24:02 +0700

    feat(constants): add constans variable

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const API_KEY = import.meta.env.VITE_META_API_KEY;
+const BASE_URL = import.meta.env.VITE_META_BASE_URL;
+const IMG_BASE_URL = import.meta.env.VITE_META_IMG_BASE_URL;
+
+export { API_KEY, BASE_URL, IMG_BASE_URL };
