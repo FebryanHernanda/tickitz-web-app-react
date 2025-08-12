55519c46f8d973e7a9b67c7bf3a804a801782806
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-12 21:52:04 +0700

    refactor(organisms/FormLogin): refactor all code and migrate to redux

--- src/components/organisms/formAuth/FormLogin.jsx
+++ src/components/organisms/formAuth/FormLogin.jsx
@@ -4,19 +4,17 @@ import { InputField } from "../../molecules";
 import { Link, useNavigate } from "react-router-dom";
 import { Eye, EyeClosed } from "lucide-react";
 import { toast } from "react-toastify";
-import { useSelector } from "react-redux";
-import { useLocalStorage } from "../../../hooks/useLocalStorage";
+import { useDispatch, useSelector } from "react-redux";
+import { setLogin } from "../../../store/slices/authSlice";
 
 const FormLogin = () => {
   const navigate = useNavigate();
-  const { userAdmin } = useSelector((state) => state.admin);
-
-  const [storageData, setStorageData] = useLocalStorage("userData");
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
 
   const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState("");
   const [password, setPassword] = useState("");
-  const [passwordError, setPasswordError] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
   const togglePassword = () => setShowPassword((prev) => !prev);
@@ -24,92 +22,50 @@ const FormLogin = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const userData = storageData;
-    let isValid = true;
-    let role = "";
+    const foundUser = userData.find((user) => user.email === email);
 
-    if (!userData && !userAdmin) {
-      isValid = false;
-      toast.error("Data tidak ditemukan!", {
-        position: "top-center",
-        autoClose: 3000,
-      });
-    } else if (userData) {
-      if (email === userData.email) {
-        setEmailError("");
-      } else {
-        setEmailError("Email tidak cocok!");
-        isValid = false;
-      }
-
-      if (password === userData.password) {
-        setPasswordError("");
-        role = "User";
-      } else {
-        setPasswordError("Password tidak cocok!");
-        isValid = false;
-      }
-    } else if (userAdmin) {
-      if (email === userAdmin.email) {
-        setEmailError("");
-      } else {
-        setEmailError("Email tidak cocok!");
-        isValid = false;
-      }
-
-      if (password === userAdmin.password) {
-        setPasswordError("");
-        role = "Admin";
-      } else {
-        setPasswordError("Password tidak cocok!");
-        isValid = false;
-      }
+    console.log(foundUser);
+    if (!foundUser) {
+      setErrorMsg("Data tidak ditemukan, Silahkan Mendaftar terlebih dahulu!");
+      return;
     }
 
-    if (isValid && role) {
-      toast.success("Login Berhasil!", {
-        position: "top-center",
-        autoClose: 1000,
-      });
+    if (foundUser.password !== password) {
+      setErrorMsg("Username/Password salah!");
+      return;
+    }
 
-      if (role === "Admin") {
-        const newUserData = {
-          ...userAdmin,
-          role: "admin",
-        };
+    dispatch(setLogin({ user: foundUser.email, role: foundUser.role }));
 
-        setStorageData(newUserData);
+    toast.success("Login Berhasil!", {
+      position: "top-center",
+      autoClose: 1000,
+    });
 
-        setTimeout(() => {
-          navigate("/admin");
-        }, 1500);
-      } else {
-        const newUserData = {
-          ...userData,
-          role: "user",
-        };
-
-        setStorageData(newUserData);
-
-        setTimeout(() => {
-          navigate("/");
-        }, 1500);
-      }
+    if (foundUser.role === "admin") {
+      setTimeout(() => {
+        navigate("/admin");
+      }, 1500);
+    } else {
+      setTimeout(() => {
+        navigate("/");
+      }, 1500);
     }
   };
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
-    setEmailError("");
+    setErrorMsg("");
   };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
-    setPasswordError("");
+    setErrorMsg("");
   };
 
   return (
     <>
+      <p className="text-center text-sm text-red-500">{errorMsg}</p>
       <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
         <InputField
           label="Email"
@@ -119,10 +75,10 @@ const FormLogin = () => {
           type="email"
           placeholder="Enter your email address!"
           required
-          value={email}
+          defaultValue={email}
           onChange={handleEmailChange}
         />
-        <p className="text-sm text-red-500">{emailError}</p>
+
         <div className="relative">
           <InputField
             label="Password"
@@ -132,7 +88,7 @@ const FormLogin = () => {
             type={showPassword ? "text" : "password"}
             placeholder="Enter your password"
             required
-            value={password}
+            defaultValue={password}
             onChange={handlePasswordChange}
           />
           <button
@@ -141,10 +97,8 @@ const FormLogin = () => {
             onClick={togglePassword}
           >
             {showPassword ? <EyeClosed size={20} /> : <Eye size={20} />}
-            {/* <Eye size={20} /> */}
           </button>
         </div>
-        <p className="text-sm text-red-500">{passwordError}</p>
 
         <MyButton type="submit">Login</MyButton>
         <Link to="/auth/register" className="text-right hover:text-blue-800">
