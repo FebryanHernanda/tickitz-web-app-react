12d8e758459f6acc4fb3ee4c184c41feedf23ac6
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-13 10:56:46 +0700

    fix(organisms/orderhistory): fix the data based

--- src/components/organisms/profileContent/OrderHistory.jsx
+++ src/components/organisms/profileContent/OrderHistory.jsx
@@ -1,17 +1,20 @@
+import { useSelector } from "react-redux";
 import { TicketHistory } from "../../molecules";
 
 const OrderHistory = () => {
-  /* Get data from localstorage */
-  const data = localStorage.getItem("userData");
-  const userData = JSON.parse(data);
+  const { userData } = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.auth);
+
+  const currentUser = userData.find((data) => data.id === user.id);
+  const historyOrder = currentUser.order;
 
   return (
     <>
       <div className="flex w-full flex-col gap-[40px]">
-        {!userData.order ? (
+        {!currentUser.order.length ? (
           <p className="text-center">You dont have order history</p>
         ) : (
-          <TicketHistory userData={userData} />
+          <TicketHistory userData={historyOrder} />
         )}
       </div>
     </>
