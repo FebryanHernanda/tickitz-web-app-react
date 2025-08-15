b6502e11697cd288437b0bf56347051af29ed3d9
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-15 15:50:39 +0700

    feat(organisms/profileContent): add validate input on account settings

--- src/components/organisms/profileContent/AccountSettings.jsx
+++ src/components/organisms/profileContent/AccountSettings.jsx
@@ -1,34 +1,165 @@
 import { Eye, EyeClosed } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  emailPattern,
+  passPattern,
+  phoneNumberPattern,
+} from "../../../utils/regex";
+import { toast } from "react-toastify";
+import { editUser } from "../../../store/slices/userSlice";
 
 const AccountSettings = () => {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.auth);
+
+  const [errorMsg, setErrorMsg] = useState({});
+
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    email: "",
+    newPassword: "",
+    confirmPassword: "",
+  });
+
+  useEffect(() => {
+    const foundUser = userData.find((data) => data.id === user.id);
+
+    const splitName = foundUser.fullName.trim().split(" ");
+    const firstName = splitName.shift();
+    const lastName = splitName.join(" ");
+
+    setCurrentUser(foundUser);
+    setFirstName(firstName);
+    setLastName(lastName);
+  }, [userData, user, setCurrentUser, setFirstName, setLastName]);
+
+  /* Check if CurrentUser have data change */
+  useEffect(() => {
+    if (currentUser) {
+      setFormData({
+        firstName: firstName || "",
+        lastName: lastName || "",
+        phoneNumber: currentUser?.phoneNumber || "",
+        email: currentUser?.email || "",
+        newPassword: currentUser?.password || "",
+        confirmPassword: currentUser?.password || "",
+      });
+    }
+  }, [currentUser, firstName, lastName]);
+
+  /* Check form valid */
+  const isFormValid = Object.values(formData).every(
+    (value) => value.trim() !== "",
+  );
 
   /* Check previous value  */
   const togglePassword = () => setShowPassword((prev) => !prev);
+  const toggleConfirmPassword = () => setShowConfirmPassword((prev) => !prev);
 
-  /* Handle AccountSettings form */
-  const handleAccountSettings = (e) => {
-    e.preventDefault();
+  const handleInput = (e) => {
+    const { name, value } = e.target;
 
-    const newPassword = e.target.newPassword.value;
-    const confirmPassword = e.target.confirmPassword.value;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
 
-    if (newPassword !== confirmPassword) {
-      alert("Password does not match");
-      return;
+    setErrorMsg({});
+  };
+
+  const validateInput = () => {
+    const newErrors = {};
+    let personalNumber;
+    let password;
+
+    /* ======================================Validate Fullname Field====================================== */
+    if (!formData.firstName.trim()) {
+      newErrors.firstName = "Kolom tidak boleh kosong!";
     }
 
-    const userData = JSON.parse(localStorage.getItem("userData"));
-    userData.password = confirmPassword;
-    localStorage.setItem("userData", JSON.stringify(userData));
+    if (!formData.lastName.trim()) {
+      newErrors.lastName = "Kolom tidak boleh kosong!";
+    }
+
+    /* ======================================Validate Email====================================== */
+    if (!formData.email.trim()) {
+      newErrors.email = "Kolom tidak boleh kosong!";
+    }
+    if (!emailPattern.test(formData.email)) {
+      newErrors.email = "Format email tidak valid!";
+    }
+
+    /* ======================================Validate Phone Number====================================== */
+    if (!formData.phoneNumber) {
+      newErrors.phoneNumber = "Kolom tidak boleh kosong!";
+    } else {
+      personalNumber = formData.phoneNumber.startsWith("62")
+        ? formData.phoneNumber
+        : `62${formData.phoneNumber}`;
+      if (!phoneNumberPattern.test(personalNumber)) {
+        newErrors.phoneNumber =
+          "Format tidak valid! Maksimal 13 karakter dan tidak diawali dengan 0. Contoh: 81345678991";
+      } else {
+        formData.phoneNumber = personalNumber;
+      }
+    }
+
+    /* ======================================Validate Password====================================== */
+
+    if (!formData.newPassword.trim() && !formData.confirmPassword.trim()) {
+      newErrors.password = "Kolom tidak boleh kosong!";
+    } else {
+      if (formData.newPassword !== formData.confirmPassword) {
+        newErrors.password = "Password tidak sama!";
+      } else {
+        password = formData.newPassword;
+        if (!passPattern.test(password)) {
+          newErrors.password =
+            "Password harus minimal 8 karakter, berisi huruf besar, huruf kecil, dan karakter spesial";
+        } else {
+          formData.password = password;
+        }
+      }
+    }
+
+    setErrorMsg(newErrors);
+
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (validateInput()) {
+      toast.success("Data berhasil diubah!", {
+        position: "top-center",
+        autoClose: 1000,
+      });
+
+      dispatch(
+        editUser({
+          userId: currentUser.id,
+          formData,
+        }),
+      );
+    }
   };
 
   return (
     <>
       <form
         className="flex h-full flex-col justify-between gap-[40px]"
-        onSubmit={handleAccountSettings}
+        onSubmit={handleSubmit}
       >
         {/* <!-- Container Account settings --> */}
         <div className="relative flex flex-col gap-[30px] overflow-hidden rounded-[24px] bg-white p-[30px]">
@@ -43,7 +174,12 @@ const AccountSettings = () => {
                   name="firstName"
                   placeholder="Jonas"
                   className="rounded-[16px] border-[1px] border-gray-400 bg-[#fcfdfe] p-[15px]"
+                  defaultValue={formData.firstName}
+                  onChange={handleInput}
                 />
+                {errorMsg.firstName && (
+                  <p className="text-sm text-red-500">{errorMsg.firstName}</p>
+                )}
               </div>
               <div className="flex w-full flex-col gap-[10px]">
                 <label htmlFor="lastName">Last Name</label>
@@ -52,7 +188,12 @@ const AccountSettings = () => {
                   name="lastName"
                   placeholder="El Rodriguez"
                   className="rounded-[16px] border-[1px] border-gray-400 bg-[#fcfdfe] p-[15px]"
+                  defaultValue={formData.lastName}
+                  onChange={handleInput}
                 />
+                {errorMsg.lastName && (
+                  <p className="text-sm text-red-500">{errorMsg.lastName}</p>
+                )}
               </div>
             </div>
             <div className="flex gap-[50px] max-[640px]:flex-wrap max-[640px]:gap-[20px]">
@@ -63,19 +204,31 @@ const AccountSettings = () => {
                   name="email"
                   placeholder="jonasrodrigu123@gmail.com"
                   className="rounded-[16px] border-[1px] border-gray-400 bg-[#fcfdfe] p-[15px]"
+                  defaultValue={formData.email}
+                  onChange={handleInput}
                 />
+                {errorMsg.email && (
+                  <p className="text-sm text-red-500">{errorMsg.email}</p>
+                )}
               </div>
-              <div className="relative flex w-full flex-col gap-[10px]">
-                <span className="absolute bottom-[6px] left-[10px] border-r-[0.5px] border-gray-400 p-[10px] text-gray-400">
-                  +62
-                </span>
-                <label htmlFor="personalNumber">Phone Number</label>
-                <input
-                  type="tel"
-                  name="personalNumber"
-                  placeholder="81445687121"
-                  className="rounded-[16px] border-[1px] border-gray-400 bg-[#fcfdfe] p-[15px] pl-[70px]"
-                />
+              <div className="flex w-full flex-col gap-[10px]">
+                <div className="relative flex flex-col gap-[10px]">
+                  <span className="absolute bottom-[6px] left-[10px] border-r-[0.5px] border-gray-400 p-[10px] text-gray-400">
+                    +62
+                  </span>
+                  <label htmlFor="phoneNumber">Phone Number</label>
+                  <input
+                    type="tel"
+                    name="phoneNumber"
+                    placeholder="81445687121"
+                    className="rounded-[16px] border-[1px] border-gray-400 bg-[#fcfdfe] p-[15px] pl-[70px]"
+                    defaultValue={formData.phoneNumber.substring(2)}
+                    onChange={handleInput}
+                  />
+                </div>
+                {errorMsg.phoneNumber && (
+                  <p className="text-sm text-red-500">{errorMsg.phoneNumber}</p>
+                )}
               </div>
             </div>
           </div>
@@ -95,6 +248,8 @@ const AccountSettings = () => {
                   name="newPassword"
                   placeholder="Write your password"
                   className="rounded-[16px] border-[1px] border-gray-400 bg-white p-[15px]"
+                  defaultValue={currentUser.password}
+                  onChange={handleInput}
                 />
                 <button
                   type="button"
@@ -107,26 +262,34 @@ const AccountSettings = () => {
               <div className="relative flex w-full flex-col gap-[10px]">
                 <label htmlFor="confirmPassword">Confirm Password</label>
                 <input
-                  type={showPassword ? "text" : "password"}
+                  type={showConfirmPassword ? "text" : "password"}
                   name="confirmPassword"
                   placeholder="Confirm your password"
                   className="rounded-[16px] border-[1px] border-gray-400 bg-white p-[15px]"
+                  defaultValue={currentUser.password}
+                  onChange={handleInput}
                 />
                 <button
                   type="button"
                   className="absolute top-13 right-5"
-                  onClick={togglePassword}
+                  onClick={toggleConfirmPassword}
                 >
-                  {showPassword ? <EyeClosed /> : <Eye />}
+                  {showConfirmPassword ? <EyeClosed /> : <Eye />}
                 </button>
               </div>
             </div>
+            {errorMsg.password && (
+              <p className="text-center text-sm text-red-500">
+                {errorMsg.password}
+              </p>
+            )}
           </div>
         </div>
         {/* <!-- Container Privacy Account settings --> */}
         <button
           type="submit"
-          className="w-[400px] rounded-[16px] border-none bg-blue-700 p-[20px] text-white max-[1025px]:w-full"
+          className={`w-[400px] rounded-[16px] border-none p-[20px] text-white max-[1025px]:w-full ${!isFormValid ? "cursor-not-allowed bg-gray-400" : "cursor-pointer bg-blue-700 hover:bg-blue-800"}`}
+          disabled={!isFormValid}
         >
           Update Changes
         </button>
