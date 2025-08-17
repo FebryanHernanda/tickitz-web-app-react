fe3ea567a437321b6fb55b0345368a238308df71
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-17 14:10:12 +0700

    feat(data/cinema): add cinemaLogo source base

---
+++ src/data/cinema/getCinemaLogo.js
@@ -0,0 +1,12 @@
+import cineOneLogo from "/src/assets/icons/sponsor/CineOne-logo.svg";
+import ebvLogo from "/src/assets/icons/sponsor/ebv-logo.svg";
+import hiflixLogo from "/src/assets/icons/sponsor/hiflix-logo.svg";
+
+/* Cinemas Icon Data */
+const cinemaLogos = {
+  cineone21: cineOneLogo,
+  ebv: ebvLogo,
+  hiflix: hiflixLogo,
+};
+
+export default cinemaLogos;
---
+++ src/data/cinema/index.js
@@ -0,0 +1,1 @@
+export { default as cinemaLogos } from "./getCinemaLogo";
