04b8ef5f3b964ac5728c8bd259c17e9d1ffbc0a6
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-17 13:56:25 +0700

    refactor(pages/movies): refactor code and add dynamic paginations feature

--- src/components/pages/movies/MoviesPages.jsx
+++ src/components/pages/movies/MoviesPages.jsx
@@ -1,34 +1,39 @@
-import { ArrowRight, Search } from "lucide-react";
+import { ArrowLeft, ArrowRight, Loader2, Search } from "lucide-react";
 import { MoviesList, Newslatters } from "../../organisms";
 
 import heroBg from "/src/assets/background/background.png";
 import { useSearchParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchMovies,
   fetchMoviesByGenres,
   fetchSearchMovies,
-  setSelectedGenres,
 } from "../../../store/slices/moviesSlice";
+import debounce from "lodash.debounce";
 
 const MoviesPages = () => {
   const dispatch = useDispatch();
 
-  const { searchResults, movies, genres, selectedGenres, loading } =
-    useSelector((state) => state.movies);
+  const { searchResults, totalPages, movies, genres, loading } = useSelector(
+    (state) => state.movies,
+  );
 
   const [searchParams, setSearchParams] = useSearchParams("");
 
   const querySearch = searchParams.get("query") || "";
   const currentPage = Number(searchParams.get("pages") || "1");
 
+  const [selectedGenres, setSelectedGenres] = useState(null);
+
   /* Reset Url Search Params */
   useEffect(() => {
-    setSearchParams("");
-    dispatch(setSelectedGenres(""));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    if (querySearch === "") {
+      const newParams = new URLSearchParams(searchParams);
+      newParams.delete("query");
+      setSearchParams(newParams);
+    }
+  }, [querySearch, setSearchParams, searchParams]);
 
   /* fetching Movies by genres */
   useEffect(() => {
@@ -37,29 +42,48 @@ const MoviesPages = () => {
     }
   }, [dispatch, selectedGenres]);
 
-  /* fetching Movies by search */
-  useEffect(() => {
-    if (querySearch && querySearch.trim() !== "") {
-      dispatch(fetchSearchMovies({ query: querySearch, page: currentPage }));
-    } else {
-      dispatch(fetchMovies({ page: currentPage }));
-    }
-  }, [dispatch, querySearch, currentPage]);
-
-  /* Handle Search */
+  /* Search Logic with debounce library */
   const handleSearch = (e) => {
     const { value } = e.target;
     setSearchParams({ query: value, page: 1 });
   };
 
+  /* Debounce function */
+  const debouncedFetch = useMemo(
+    () =>
+      debounce((search, page) => {
+        if (search && search.trim() !== "") {
+          dispatch(fetchSearchMovies({ query: search, page }));
+        } else {
+          dispatch(fetchMovies({ page }));
+        }
+      }, 1000),
+    [dispatch],
+  );
+
+  /* clear debounce */
+  useEffect(() => {
+    debouncedFetch(querySearch, currentPage);
+    return () => {
+      debouncedFetch.cancel();
+    };
+  }, [querySearch, currentPage, debouncedFetch]);
+
   /* Handle Paginations */
   const handlePaginations = (page) => {
+    if (page < 1 || page > totalPages) return;
     setSearchParams({ query: querySearch, pages: page });
+    debouncedFetch(querySearch, page);
   };
+
+  /* Handle dynamic pages */
+  const start = Math.max(1, currentPage - 2);
+  const end = Math.min(totalPages, currentPage + 2);
+  const pages = Array.from({ length: end - start + 1 }, (_, i) => start + i);
 
   /* Handle Filter */
   const handleFilter = (genreId, value) => {
-    dispatch(setSelectedGenres(genreId));
+    setSelectedGenres(genreId);
     setSearchParams({ genres: value.toLowerCase() });
   };
 
@@ -127,25 +151,29 @@ const MoviesPages = () => {
               </div>
 
               <div className="flex w-full flex-wrap justify-between gap-5">
-                {querySearch === "" ||
-                querySearch === null ||
-                querySearch === undefined ||
-                searchResults.length > 0 ? (
-                  <MoviesList
-                    movies={moviesData}
-                    genres={genres}
-                    loading={loading}
-                  />
-                ) : (
+                {loading ? (
+                  <div className="flex w-full justify-center py-10">
+                    <Loader2 className="h-10 w-10 animate-spin text-blue-500" />
+                  </div>
+                ) : searchResults?.length === 0 && querySearch ? (
                   <p className="w-full text-center text-black">
                     Film tidak ditemukan!
                   </p>
+                ) : (
+                  <MoviesList movies={moviesData} genres={genres} />
                 )}
               </div>
 
               {/* <!-- Next list  --> */}
               <div className="flex items-center justify-center gap-5">
-                {[1, 2, 3, 4, 5].map((page) => {
+                <button
+                  className={`flex h-10 w-10 items-center justify-center rounded-full text-white ${currentPage === 1 ? "!cursor-not-allowed bg-gray-400" : "bg-blue-600"} `}
+                  onClick={() => handlePaginations(currentPage - 1)}
+                  disabled={currentPage === 1}
+                >
+                  <ArrowLeft />
+                </button>
+                {pages.map((page) => {
                   return (
                     <button
                       key={page}
@@ -157,7 +185,11 @@ const MoviesPages = () => {
                   );
                 })}
 
-                <button className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-600 text-white">
+                <button
+                  className={`flex h-10 w-10 items-center justify-center rounded-full text-white ${currentPage === 5 ? "!cursor-not-allowed bg-gray-400" : "bg-blue-600"} `}
+                  onClick={() => handlePaginations(currentPage + 1)}
+                  disabled={currentPage === totalPages}
+                >
                   <ArrowRight />
                 </button>
               </div>
