a9e5ceb91d041f2c62e164aa00d8e808d6ce2bb5
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-17 13:54:53 +0700

    fix(organisms/movieList): remove unused code

--- src/components/organisms/movieList/MoviesList.jsx
+++ src/components/organisms/movieList/MoviesList.jsx
@@ -1,11 +1,11 @@
 import { MovieCard } from "../../molecules";
+import { Loader2 } from "lucide-react";
 
-const MovieList = ({ limits, movies, genres, loading }) => {
+const MovieList = ({ limits, movies, genres }) => {
   const displayedMovies = limits ? movies.slice(0, limits) : movies;
 
   return (
     <>
-      {loading ? <p className="w-full items-center">Loading...</p> : null}
       {displayedMovies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} genres={genres} />
       ))}
