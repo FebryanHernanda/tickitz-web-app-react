0dfbb6d6e025a260de4bda66d47c32b04e5f7426
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-17 14:09:50 +0700

    fix(data/movies): remove unused files

--- src/data/movies/index.js
+++ src/data/movies/index.js
@@ -1,4 +1,2 @@
-export { default as getPopularMovies } from "./getPopularMovies.js";
-export { default as getGenreMovies } from "./getGenreMovies.js";
 export { default as getDetailsMovies } from "./getDetailsMovies.js";
 export { default as getCreditsMovies } from "./getCreditsMovies.js";
