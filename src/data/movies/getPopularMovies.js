d8c6792d4a32871e0b7b42fcc3eb45dbb1029420
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-17 14:09:29 +0700

    fix(data/movies): remove unused files

--- src/data/movies/getPopularMovies.js
+++
@@ -1,9 +0,0 @@
-import fetchData from "../../services/api";
-
-const getPopularMovies = async (apiKey) => {
-  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
-  const data = await fetchData(url);
-  return data.results;
-};
-
-export default getPopularMovies;
