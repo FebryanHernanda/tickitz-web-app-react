32956325cb9be64ea21a61e0a1d215b4febdad43
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-17 13:45:33 +0700

    fix(molecules/buttonSocialMedia): fix the styling and set the width

--- src/components/molecules/buttonSocialMedia/ButtonSocialMedia.jsx
+++ src/components/molecules/buttonSocialMedia/ButtonSocialMedia.jsx
@@ -5,11 +5,11 @@ const ButtonSocialMedia = () => {
   return (
     <>
       <div className="flex justify-between">
-        <button className="flex items-center justify-center gap-5 rounded-xl border-1 border-gray-300 p-3 shadow-md hover:bg-gray-100 lg:w-50">
+        <button className="flex w-40 items-center justify-center gap-5 rounded-xl border-1 border-gray-300 p-3 shadow-md hover:bg-gray-100 lg:w-50">
           <img src={googleLogo} alt="Google Icon" className="w-10" />
           Google
         </button>
-        <button className="flex items-center justify-center gap-5 rounded-xl border-1 border-gray-300 p-3 shadow-md hover:bg-gray-100 lg:w-50">
+        <button className="flex w-40 items-center justify-center gap-5 rounded-xl border-1 border-gray-300 p-3 shadow-md hover:bg-gray-100 lg:w-50">
           <img src={facebookLogo} alt="Facebook Icon" className="w-10" />
           Facebook
         </button>
