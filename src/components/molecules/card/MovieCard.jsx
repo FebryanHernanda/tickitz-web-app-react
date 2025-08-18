fadd842a6bc7671630a7d719cd816f0fda21e95d
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-18 13:22:00 +0700

    fix(molecules/card): fix card style layout and change genre to only show 3 items

--- src/components/molecules/card/MovieCard.jsx
+++ src/components/molecules/card/MovieCard.jsx
@@ -8,15 +8,15 @@ const MovieCard = ({ movie, genres }) => {
       const found = genres.find((genre) => genre.id === id);
       return found;
     })
-    .slice(0, 5);
+    .slice(0, 3);
 
   const handleDetails = () => {
     navigate(`/movies/details/${movie.id}`);
   };
 
   return (
-    <div className="relative w-75 overflow-hidden rounded-2xl lg:w-[300px]">
-      {/* Gambar Movie */}
+    <div className="flex h-fit w-full flex-col gap-5 rounded-2xl sm:w-75 lg:w-[300px]">
+      {/* Movie Pictures */}
       <div className="relative">
         <img
           src={
@@ -29,15 +29,17 @@ const MovieCard = ({ movie, genres }) => {
         />
 
         {movie.vote_average >= 7.5 && (
-          <span className="absolute top-7 left-0 rounded-r-xl bg-blue-700 px-3 py-1 text-xs font-semibold text-white shadow shadow-2xs">
+          <span className="absolute top-7 left-0 rounded-r-xl bg-blue-700 px-3 py-1 text-xs font-semibold text-white shadow-2xl">
             Recommended
           </span>
         )}
 
         <div className="group absolute inset-0 flex items-center justify-center rounded-2xl">
-          <div className="absolute inset-0 z-0 rounded-2xl bg-black opacity-0 transition duration-300 group-hover:opacity-70"></div>
+          {/* Overlay hitam */}
+          <div className="absolute inset-0 z-0 rounded-2xl bg-black opacity-0 transition duration-300 md:opacity-0 lg:group-hover:opacity-70"></div>
 
-          <div className="z-10 flex flex-col gap-2 space-x-2 opacity-0 transition duration-300 group-hover:opacity-100">
+          {/* Tombol */}
+          <div className="z-10 hidden flex-col gap-2 opacity-100 transition duration-300 lg:flex lg:opacity-0 lg:group-hover:opacity-100">
             <button
               className="w-full rounded-md bg-white px-4 py-2 text-sm text-black transition hover:bg-gray-200"
               onClick={handleDetails}
@@ -51,11 +53,9 @@ const MovieCard = ({ movie, genres }) => {
         </div>
       </div>
 
-      {/* Deskripsi Movie */}
-      <div className="flex h-[120px] flex-col justify-between px-2 py-4">
-        <h3 className="mb-2 text-lg font-semibold text-gray-800">
-          {movie.title}
-        </h3>
+      {/* Movie Description */}
+      <div className="flex flex-col justify-between gap-5 px-2 pt-1 lg:h-fit">
+        <h3 className="text-lg font-semibold text-gray-800">{movie.title}</h3>
         <div className="flex flex-wrap gap-2">
           {findGenres &&
             findGenres.length > 0 &&
@@ -69,6 +69,18 @@ const MovieCard = ({ movie, genres }) => {
                   {genre.name}
                 </span>
               ))}
+        </div>
+
+        <div className="flex gap-2 lg:hidden">
+          <button
+            className="w-30 rounded-md border-1 border-blue-600 py-1 text-sm text-blue-700 transition hover:bg-blue-700 hover:text-white"
+            onClick={handleDetails}
+          >
+            Details
+          </button>
+          <button className="w-30 rounded-md bg-blue-600 py-1 text-sm text-white transition hover:bg-blue-700">
+            Buy Tickets
+          </button>
         </div>
       </div>
     </div>
