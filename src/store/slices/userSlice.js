c79c64c594d9c160d41b00bb1aa4d22cb4d4c0a8
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-18 09:50:53 +0700

    refactor(store/userSlice): refactor code and integrations data from database

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,20 +1,95 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
-  userData: [
-    {
-      id: 1,
-      email: "admin@gmail.com",
-      password: "@Admin12345",
-      role: "admin",
-    },
-  ],
+  data: null,
+  loading: false,
+  error: null,
 };
+
+export const getProfile = createAsyncThunk(
+  "user/profile/data",
+  async (_, { getState, rejectWithValue }) => {
+    try {
+      const token = getState().auth.token;
+
+      const response = await axios.get("http://127.0.0.1:8080/profile", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
+
+export const updateProfile = createAsyncThunk(
+  "user/profile",
+  async (fileData, { getState, rejectWithValue }) => {
+    try {
+      const token = getState().auth.token;
+      console.log(fileData);
+
+      const formData = new FormData();
+      formData.append("first_name", fileData.firstName);
+      formData.append("last_name", fileData.lastName);
+      formData.append("phone_number", fileData.phoneNumber);
+      formData.append("email", fileData.email);
+      formData.append("image", fileData.avatarPath);
+
+      const response = await axios.patch(
+        "http://127.0.0.1:8080/profile/edit",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        },
+      );
+
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data?.error || "Update gagal");
+    }
+  },
+);
+
+export const updatePassword = createAsyncThunk(
+  "user/profile/password",
+  async (fileData, { getState, rejectWithValue }) => {
+    try {
+      const token = getState().auth.token;
+
+      const formData = new FormData();
+      formData.append("old_password", fileData.oldPassword);
+      formData.append("new_password", fileData.newPassword);
+
+      const response = await axios.patch(
+        "http://127.0.0.1:8080/profile/editpassword",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        },
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data?.error || "Update gagal");
+    }
+  },
+);
 
 const userSlice = createSlice({
   initialState,
   name: "users",
   reducers: {
+    resetData: () => initialState,
     addUser: (state, action) => {
       state.userData.push(action.payload);
     },
@@ -72,9 +147,56 @@ const userSlice = createSlice({
       }
     },
   },
+  extraReducers: (builder) => {
+    builder
+      /* =========================================== Get Data =========================================== */
+      .addCase(getProfile.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getProfile.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(getProfile.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      /* =========================================== Update =========================================== */
+      .addCase(updateProfile.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateProfile.fulfilled, (state) => {
+        state.loading = false;
+        // state.userData = action.payload;
+      })
+      .addCase(updateProfile.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      /* =========================================== Update Password =========================================== */
+      .addCase(updatePassword.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updatePassword.fulfilled, (state) => {
+        state.loading = false;
+      })
+      .addCase(updatePassword.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
-export const { addUser, addOrder, updatePaymentData, editUser, resetPassword } =
-  userSlice.actions;
+export const {
+  addUser,
+  addOrder,
+  updatePaymentData,
+  editUser,
+  resetPassword,
+  resetData,
+} = userSlice.actions;
 
 export default userSlice.reducer;
