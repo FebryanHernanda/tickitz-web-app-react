ed8d909af572d11098ebf487f89dff059158eded
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-18 22:42:42 +0700

    refactor(organisms/movieList): refactor code with new data from BE

--- src/components/organisms/movieList/MoviesList.jsx
+++ src/components/organisms/movieList/MoviesList.jsx
@@ -1,13 +1,13 @@
 import { MovieCard } from "../../molecules";
 import { Loader2 } from "lucide-react";
 
-const MovieList = ({ limits, movies, genres }) => {
+const MovieList = ({ limits, movies }) => {
   const displayedMovies = limits ? movies.slice(0, limits) : movies;
 
   return (
     <>
       {displayedMovies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} genres={genres} />
+        <MovieCard key={movie.id} movie={movie} />
       ))}
     </>
   );
