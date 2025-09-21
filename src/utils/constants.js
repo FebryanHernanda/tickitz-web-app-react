50b022879d75b1a919d67d31876db46e352c4ba3
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-21 19:40:35 +0700

    feat(constants): add api_url load env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,6 @@ const API_KEY = import.meta.env.VITE_META_API_KEY;
 const BASE_URL = import.meta.env.VITE_META_BASE_URL;
 const IMG_BASE_URL = import.meta.env.VITE_META_IMG_BASE_URL;
 
-export { API_KEY, BASE_URL, IMG_BASE_URL };
+const API_URL = import.meta.env.VITE_API_URL;
+
+export { API_KEY, BASE_URL, IMG_BASE_URL, API_URL };
